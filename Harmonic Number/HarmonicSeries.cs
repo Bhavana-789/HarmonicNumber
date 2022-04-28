3a641a8e36a86cd08aca431ef5f149e586dc03e0
Bhavana <bhavananerlakota@gmail.com>
2022-04-28 09:02:45 +0530

    [Bhavana]:Added Harmonic program

---
+++ Harmonic Number/HarmonicSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonic_Number
+{
+    internal class HarmonicSeries
+    {
+        public void Harmonic()
+        {
+            Console.WriteLine("Enter Number which you want Harmonic value");
+            int num=Convert.ToInt32(Console.ReadLine());
+            double result = 0;
+            for(int i=1;i<=num;i++)
+            {
+                result += (1 / (Convert.ToDouble(i)));
+                if (i==num)
+                {
+                    Console.WriteLine($"1 /{i}= ");
+                }
+                else
+                {
+                    Console.WriteLine($"1/{i}+ ");
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Harmonic Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Harmonic_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HarmonicSeries harmonicSeries = new HarmonicSeries();
+            harmonicSeries.Harmonic();
+        }
+    }
+}
